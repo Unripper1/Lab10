3c398869ea29e5ee50e73e85beb84a1d2cec94f7
Unripper1 <dmharitoh@gmail.com>
2021-02-25 19:12:25 +0300

    com2

--- src/Null.java
+++ src/Null.java
@@ -6,6 +6,14 @@ public class Null {
         if(a==0 || b==0) return 1;
         return f(a,b-1)+f(a-1,b-1);
     }
+
+
+
+
+
+
+
+    
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
         int a=in.nextInt();
